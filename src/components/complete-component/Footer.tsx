7aa75239f06d110565fc1c25ea45ac83aa167723
Agostinho <gelsonma266@gmail.com>
2025-12-27 12:48:49 +0100

    Redes socias adicicionadas com sucessso

--- src/components/complete-component/Footer.tsx
+++ src/components/complete-component/Footer.tsx
@@ -1,4 +1,5 @@
 import FooterDetails from "../short-components/FooterDetails";
+import SocialMedia from "../short-components/SocialMedias";
 const Footer = ()=>{
     const helpLinks:string[] = ['Link Chat','Call Us', 'Text Us', 'helP@allbirds.com','FAQ/Contact Us', 'Returns/Exchanges']
     const shopLinks:string[] = ['Men`s Shoes', 'Women`s Shoes', 'Men`s Apperer', 'Womne`s Apperer']
@@ -12,7 +13,11 @@ const Footer = ()=>{
                     <input className="px-4 py-2 rounded-s-xl w-full placeholder:text-gray-600" type="text" placeholder="Email Adress..." />
                     <button className="text-black pe-3 rounded-e-xl bg-white">Subscribe</button>
                     </div>
+                    <div>
+                        <SocialMedia />
+                    </div>
                 </div>
+                
                 <div className="flex mt-20 space-y-6 justify-between flex-wrap p-4">
                 <FooterDetails title="Help" link={helpLinks} text_color="text-white"/>
                 <FooterDetails title="Shop" link={shopLinks} text_color="text-white"/>
--- src/components/short-components/SocialMedias.tsx
+++ src/components/short-components/SocialMedias.tsx
@@ -1,9 +1,21 @@
-const SocialNedias = ()=>{
+const SocialMedia = ()=>{
     return (
         <>
-            <div className="flex space-x-4 p-4 rounded-lg bg-gray-900">
-                
+            <div className="flex space-x-4 p-4 rounded-lg bg-zinc-950 w-fit">
+                <a href="#" className="text-white transition-opacity hover:opacity-65 text-xl" aria-label="Instagram" title="Instagram">
+                <i className='bx bxl-instagram'></i>
+                </a>
+                <a href="#" className="text-white transition-opacity hover:opacity-65 text-xl" aria-label="Git Hub" title="Git Hub">
+                <i className='bx bxl-github'></i>
+                </a>
+                <a href="#" className="text-white transition-opacity hover:opacity-65 text-xl" aria-label="Linkedin" title="Linkedin">
+                    <i className='bx bxl-linkedin'></i>
+                </a>
+                <a href="#" className="text-white transition-opacity hover:opacity-65 text-xl" aria-label="Tik Tok" title="Tik Tok">
+                <i className='bx bxl-tiktok'></i>
+                </a>   
             </div>
         </>
     )
 }
+export default SocialMedia;
